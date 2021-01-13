920fc11d4358e0c33b45fd5235aa13635f47ea28
= <nehal.chigurupati@gmail.com>
2021-01-13 15:57:53 -0500

    Edit imports to settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,7 @@ import os
 import sys
 from decouple import config, Csv
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
